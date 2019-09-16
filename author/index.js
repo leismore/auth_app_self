7860bb786f29e535497055bb52fc05b7b304cb6f
Kyle Chine <kyle@leismore.co>
2019-09-16 17:16:03 +1000

    add AuthorInputs class

---
+++ author/index.js
@@ -0,0 +1,10 @@
+/**
+ * The Authorization API -- /api/authorizer
+ *
+ * @res.locals
+ * {
+ *   AuthorError:    {AuthorError}
+ *   inputs:         {AuthorInputs object}
+ *   db:             {nano}
+ * }
+ */
