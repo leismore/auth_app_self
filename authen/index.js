6639b79192ae85551125eeca25eb6f403a425e1d
Kyle Chine <kyle@leismore.co>
2019-09-27 13:45:42 +1000

    get & all handlers added - index update

--- authen/index.js
+++ authen/index.js
@@ -13,3 +13,6 @@
 
 exports.post_handler1 = require('./post_handler1');
 exports.post_handler2 = require('./post_handler2');
+
+exports.get_handler1  = require('./get_handler1');
+exports.all_handler1  = require('./all_handler1');
--- author/index.js
+++ author/index.js
@@ -14,3 +14,6 @@
 exports.post_handler1 = require('./post_handler1');
 exports.post_handler2 = require('./post_handler2');
 exports.post_handler3 = require('./post_handler3');
+
+exports.get_handler1  = require('./get_handler1');
+exports.all_handler1  = require('./all_handler1');
