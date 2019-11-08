a3f3c0e6e2a46703560969d05e653be6672bc744
Kyle Chine <kyle@leismore.co>
2019-11-09 10:50:56 +1100

    fixed: AuthorResponse, lib clear

--- src/lib/AuthorResponse.ts
+++ src/lib/AuthorResponse.ts
@@ -3,23 +3,18 @@
  * Refer to @leismore/response <https://www.npmjs.com/package/@leismore/response>
  */
 
-'use strict';
-
-const Response    = require('@leismore/response');
-const AuthorError = require('./AuthorError');
+import { Response } from '@leismore/response';
 
 class AuthorResponse extends Response
 {
-  res200(result)
+  public res200(result:boolean):void
   {
-    if (typeof result !== 'boolean')
-    {
-      throw new AuthorError('not boolean', 1, 500);
-    }
-
-    this.res.set('Content-Type', 'application/json');
-    this.res.status(200).send({'result': result});
+    this.send({
+      statusCode: '200',
+      headers: {'Content-Type': 'application/json'},
+      body: {'result': result}
+    });
   }
 }
 
-module.exports = AuthorResponse;
+export { AuthorResponse };
