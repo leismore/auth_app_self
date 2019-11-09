b326598bfac26f3e9fab54ae820fcc5925b01738
Kyle Chine <kyle@leismore.co>
2019-11-10 09:14:07 +1100

    res.locals types deleted

--- src/lib/type/AuthenLocals.ts
+++
@@ -1,9 +0,0 @@
-// res.locals for authentication API
-
-import { AuthenInputs } from '../AuthenInputs';
-
-type AuthenLocals = {
-  inputs?: AuthenInputs
-};
-
-export { AuthenLocals };
--- src/lib/type/AuthorLocals.ts
+++
@@ -1,9 +0,0 @@
-// res.locals for authorization API
-
-import { AuthorInputs } from '../AuthorInputs';
-
-type AuthorLocals = {
-  inputs?: AuthorInputs
-};
-
-export { AuthorLocals };
