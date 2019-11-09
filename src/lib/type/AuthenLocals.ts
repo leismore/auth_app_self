016f8661264f3bad2ee048f561d8311a3bcaec14
Kyle Chine <kyle@leismore.co>
2019-11-10 09:07:25 +1100

    res.locals types added

---
+++ src/lib/type/AuthenLocals.ts
@@ -0,0 +1,9 @@
+// res.locals for authentication API
+
+import { AuthenInputs } from '../AuthenInputs';
+
+type AuthenLocals = {
+  inputs?: AuthenInputs
+};
+
+export { AuthenLocals };
---
+++ src/lib/type/AuthorLocals.ts
@@ -0,0 +1,9 @@
+// res.locals for authorization API
+
+import { AuthorInputs } from '../AuthorInputs';
+
+type AuthorLocals = {
+  inputs?: AuthorInputs
+};
+
+export { AuthorLocals };
