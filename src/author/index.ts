3182f42614cc01f215e60f5627fbae2493713c97
Kyle Chine <kyle@leismore.co>
2019-11-11 21:07:19 +1100

    author done

--- src/author/index.ts
+++ src/author/index.ts
@@ -7,11 +7,10 @@
  * }
  */
 
-'use strict';
+import { all_handler1 }  from './all_handler1';
+import { get_handler1 }  from './get_handler1';
+import { post_handler1 } from './post_handler1';
+import { post_handler2 } from './post_handler2';
+import { post_handler3 } from './post_handler3';
 
-exports.post_handler1 = require('./post_handler1');
-exports.post_handler2 = require('./post_handler2');
-exports.post_handler3 = require('./post_handler3');
-
-exports.get_handler1  = require('./get_handler1');
-exports.all_handler1  = require('./all_handler1');
+export { all_handler1, get_handler1, post_handler1, post_handler2, post_handler3 };
