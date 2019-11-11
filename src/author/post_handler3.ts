23c97274dec843edca4ee2019c390eb15e076085
Kyle Chine <kyle@leismore.co>
2019-11-11 21:03:56 +1100

    post3 done; author

--- src/author/post_handler3.ts
+++ src/author/post_handler3.ts
@@ -2,35 +2,36 @@
  * POST handler 3 - Authorization
  */
 
-'use strict';
+import * as express       from 'express';
+import * as nano          from 'nano';
+import { AuthorError }    from '../lib/AuthorError';
+import { AuthorInputs }   from '../lib/AuthorInputs';
+import { AuthorResponse } from '../lib/AuthorResponse';
+import { AuthorDoc }      from '../lib/type/db_doc_author';
+import { connect_db }     from '../lib/connect_db';
+import { get_permission } from '../lib/get_permission';
+import * as CONFIG        from '../config.json';
+const DB_NAME = CONFIG.couchdb.dbPrefix + '_private_app_authorization';
 
-const CONFIG         = require('../config.json');
-const DB_NAME        = CONFIG.couchdb.dbPrefix + '_private_app_authorization';
-const connect_db     = require('../lib/connect_db');
-const get_permission = require('../lib/get_permission');
-const AuthorResponse = require('../lib/AuthorResponse');
-
-function post_handler3(req, res, next)
+function post_handler3(_req:express.Request, res:express.Response, next:express.NextFunction):void
 {
-  const AuthorError = res.locals.AuthorError;
-  const inputs      = res.locals.inputs;
-  const resp        = new AuthorResponse(res);
-  let r             = null;
-  let db            = null;
+  const inputs:AuthorInputs = res.locals.inputs;
+  const resp                = new AuthorResponse(res);
+  let   db:nano.DocumentScope<AuthorDoc>;
 
   // Connect to DB
   try {
     db = connect_db().use(DB_NAME);
   } catch (e) {
-    next( new AuthorError('connect CouchDB failure', 6, 500, e) );
+    let error = {message: 'CouchDB: connection failure', code: '2'};
+    let response = {statusCode: '500'};
+    next( new AuthorError(error, response, e) );
     return;
   }
 
   // Get data from DB
   get_permission(db, [inputs.data.clientID, inputs.data.hostID, inputs.data.permission])
-
   .then( r => {
-
     if (r.rows.length === 0 || r.rows[0].value === false)
     {
       resp.res200(false);
@@ -41,14 +42,14 @@ function post_handler3(req, res, next)
       resp.res200(true);
       return;
     }
-
   })
-
   .catch( e => {
-    next( new AuthorError('CouchDB: get_permission failure', 7, 500, e) );
+    let error = {message: 'CouchDB (View): get_permission failure', code: '3'};
+    let response = {statusCode: '500'};
+    next( new AuthorError(error, response, e) );
     return;
   });
 
 }
 
-module.exports = post_handler3;
+export { post_handler3 };
